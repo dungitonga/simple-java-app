383a967af47dc0b153c5b470abc1fffdeb03535b
Duncan <dungitonga.can@gmail.com>
2025-05-29 19:34:48 +0000

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+// App.java
+package com.example;
+
+/**
+ * A simple Java application to demonstrate Maven build.
+ */
+public class App {
+    public static void main(String[] args) {
+        // Print a greeting message to the console
+        System.out.println("Hello, Maven!");
+    }
+}
